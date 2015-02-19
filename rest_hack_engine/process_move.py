d16a4247b89e6d4536cd2a80756da30ad7a87e3e
Kirk Freiheit <kirk.freiheit@gmail.com>
2015-02-19 17:23:31 -0500

    100% code coverage, more tests added.

--- rest_hack_engine/process_move.py
+++ rest_hack_engine/process_move.py
@@ -1,5 +1,5 @@
 import json
-
+import sys
 
 def process_move(gameboard, moves_requested):
     """
@@ -12,42 +12,123 @@ def process_move(gameboard, moves_requested):
        tuple of gameboard, moves_processed, moves_with_errors
     """
 
-    board = json.loads(gameboard)
+    result_board = json.loads(gameboard)
     # TODO: validate gameboard state
 
     # note: moves are validated in the loops below
     moves = json.loads(moves_requested)
 
-    result_board = board
     result_moves_processed = {'moves': []}
     result_moves_with_errors = {'moves': []}
 
+    node_dict = {}
+
+    # Coerce int properties to be ints:
+    for node in result_board['nodes']:
+        node['id'] = int(node['id'])
+        node['points'] = int(node['points'])
+        node_dict[node['id']] = node
+
     for move in moves['moves']:
+        try:
+            move['points'] = int(move['points'])
+            move['node_from'] = int(move['node_from'])
+            move['node_to'] = int(move['node_to'])
+            result_moves_processed['moves'].append(move)
+        except Exception:
+            result_moves_with_errors['moves'].append(move)
+
+    iterable_moves_list = result_moves_processed['moves'][:]
+    result_moves_processed = {'moves': []}
+    for move in iterable_moves_list:
         # print "processing move..."
         # print "node_from : {0}".format(move['node_from'])
         # print "node_to : {0}".format(move['node_to'])
         # print "owner : {0}".format(move['owner'])
         # print "points : {0}".format(move['points'])
 
-        node_from_id = move['node_from']
-        node_to_id = move['node_to']
+        try:
+            from_node = node_dict[move['node_from']]
+            to_node = node_dict[move['node_to']]
+        except Exception:
+            result_moves_with_errors['moves'].append(move)
+            continue
 
-        from_node = None
-        to_node = None
-        for node in board['nodes']:
-            if node['id'] == node_from_id:
-                from_node = node
-
-            if node['id'] == node_to_id:
-                to_node = node
-
-        if from_node and to_node:
-            # print "move from-to path OK"
-            result_moves_processed['moves'].append(move)
+        # First pass does two things:
+        # - Does all checks, fails invalid moves by putting them into result_moves_with_errors
+        # - For all valid moves, the points moving are removed from the FROM node.
+        #   These moves are stored in result_moves_processed.
+        #
+        # Second pass (through the result_moves_processed) is where the bloodbath ensues, and
+        # final TO node owner/points are calculated and assigned.
+        #
+        # This first pass just sets the stage (puts the valid moves' points in motion)
+        # path is ok
+        if from_node['owner'] == move['owner']:
+            # owner is right
+            points_remaining = from_node['points'] - move['points']
+            if points_remaining >= 0:
+                from_node['points'] = points_remaining
+                result_moves_processed['moves'].append(move)
+            else:
+                # not enough points in the from_node
+                result_moves_with_errors['moves'].append(move)
         else:
-            # print "invalid path: {0}-{1}".format(node_from_id, node_to_id)
+            # wrong owner
             result_moves_with_errors['moves'].append(move)
 
-    # TODO: actually process the moves and make a result_board
+    # Second pass determines winner of each node, sets points and owner accordingly:
+    # dictionary of node_id -> dict(owner, points)
+    node_fight_dict = {}
 
+    # Collect all moving armies to the fight for the destination node
+    for move in result_moves_processed['moves']:
+        player_move = {'owner': move['owner'], 'points': move['points']}
+        key = move['node_to']
+        if node_fight_dict.has_key(key):
+            node_fight_dict[key].append(player_move)
+        else:
+            node_fight_dict[key] = [player_move]
+
+    # Add current owners to the fights:
+    for destination_id in node_fight_dict.keys():
+        node_fight_dict[destination_id].append(
+            {'owner': node_dict[destination_id]['owner'], 'points': node_dict[destination_id]['points']})
+
+    # Determine winners:
+    for node_id in node_fight_dict.keys():
+        top_owner = None
+        top_points = 0
+        for test_move in node_fight_dict[node_id]:
+            if test_move['points'] > top_points:
+                top_points = test_move['points']
+                top_owner = test_move['owner']
+            # else:
+            #     print "missed"
+
+
+        # need to find minimum value that's >= 0 --- that's the largest army minus the second largest (in points)
+        leftover_points = 0
+        for test_move in node_fight_dict[node_id]:
+            test_points = top_points - test_move['points']
+            if test_points >= leftover_points:
+                leftover_points = test_points
+            # else:
+            #     print "missed"
+
+        # Update destination node attributes:
+        # Only change owner if someone is left standing.  If zeroed out, original owner retains control of the node:
+        if leftover_points > 0:
+            node_dict[node_id]['owner'] = top_owner
+        # else:
+        #     print "missed"
+
+
+        node_dict[node_id]['points'] = leftover_points
+
+        # print 'node_id: {0}\npoints: {1}\nowner: {2}'.format(
+        #     node_dict[node_id], node_dict[node_id]['points'], node_dict[node_id]['owner'])
+        # sys.stdout.flush()
+
+    # Everyone with a dog in the fight is known; reduce and apply final values to nodes:
     return result_board, result_moves_processed, result_moves_with_errors
--- rest_hack_engine/unit_test/test_process_move.py
+++ rest_hack_engine/unit_test/test_process_move.py
@@ -48,7 +48,8 @@ class TestRestHackEngine(unittest.TestCase):
         self.assertDictEqual(moves_processed, {'moves': []})
         self.assertDictEqual(moves_with_errors, {'moves': []})
 
-    def test_check_one_valid_move(self):
+
+    def test_check_one_invalid_move_missing_node_from(self):
         """
         """
         board, moves_processed, moves_with_errors = engine.process_move(
@@ -56,24 +57,19 @@ class TestRestHackEngine(unittest.TestCase):
                   "id" : 1,
                   "owner" : "player_1",
                   "points" : "100",
-                  "connections" : [
-                    2
-                  ],
+                  "connections" : [2],
                   "starting_node" : "player_1"
                 }, {
                   "id" : 2,
                   "owner" : "player_2",
                   "points" : "100",
-                  "connections" : [
-                    1
-                  ],
+                  "connections" : [1],
                   "starting_node" : "player_2"
                 }]}''',
             '''{"moves" : [{
-                  "node_from" : 1,
-                  "node_to" : 2,
+                  "node_to" : 3,
                   "owner" : "player_1",
-                  "points" : "10"
+                  "points" : "100"
                 }]}'''
         )
 
@@ -81,32 +77,82 @@ class TestRestHackEngine(unittest.TestCase):
             {
                 u'id': 1,
                 u'owner': u'player_1',
-                u'points': u'90',
+                u'points': 100,
                 u'connections': [2],
                 u'starting_node': u'player_1'
             },
             {
-                u'id': 2,
-                u'owner': u'player_2',
-                u'points': u'90',
+                u'starting_node': u'player_2',
                 u'connections': [1],
-                u'starting_node': u'player_2'
+                u'points': 100,
+                u'owner': u'player_2',
+                u'id': 2
             }
         ]})
 
-        self.assertDictEqual(moves_processed, {u'moves': [
+        self.assertDictEqual(moves_processed, {u'moves': []})
+
+        self.assertDictEqual(moves_with_errors, {u'moves': [
+            {
+                u'node_to': 3,
+                u'owner': u'player_1',
+                u'points': 100
+            }
+        ]})
+
+
+    def test_check_one_invalid_move_missing_node_to(self):
+        """
+        """
+        board, moves_processed, moves_with_errors = engine.process_move(
+            '''{"nodes" : [{
+                  "id" : 1,
+                  "owner" : "player_1",
+                  "points" : "100",
+                  "connections" : [2],
+                  "starting_node" : "player_1"
+                }, {
+                  "id" : 2,
+                  "owner" : "player_2",
+                  "points" : "100",
+                  "connections" : [1],
+                  "starting_node" : "player_2"
+                }]}''',
+            '''{"moves" : [{
+                  "node_from" : 1,
+                  "owner" : "player_1",
+                  "points" : "100"
+                }]}'''
+        )
+
+        self.assertDictEqual(board, {u'nodes': [
+            {
+                u'id': 1,
+                u'owner': u'player_1',
+                u'points': 100,
+                u'connections': [2],
+                u'starting_node': u'player_1'
+            },
+            {
+                u'starting_node': u'player_2',
+                u'connections': [1],
+                u'points': 100,
+                u'owner': u'player_2',
+                u'id': 2
+            }
+        ]})
+
+        self.assertDictEqual(moves_processed, {u'moves': []})
+
+        self.assertDictEqual(moves_with_errors, {u'moves': [
             {
                 u'node_from': 1,
-                u'node_to': 2,
                 u'owner': u'player_1',
-                u'points': u'10'
+                u'points': 100
             }
         ]})
 
-        self.assertDictEqual(moves_with_errors, {'moves': []})
-
-
-    def test_check_one_invalid_move(self):
+    def test_check_one_invalid_move_target(self):
         """
         """
         board, moves_processed, moves_with_errors = engine.process_move(
@@ -135,14 +181,14 @@ class TestRestHackEngine(unittest.TestCase):
             {
                 u'id': 1,
                 u'owner': u'player_1',
-                u'points': u'100',
+                u'points': 100,
                 u'connections': [2],
                 u'starting_node': u'player_1'
             },
             {
                 u'starting_node': u'player_2',
                 u'connections': [1],
-                u'points': u'100',
+                u'points': 100,
                 u'owner': u'player_2',
                 u'id': 2
             }
@@ -155,6 +201,268 @@ class TestRestHackEngine(unittest.TestCase):
                 u'node_from': 1,
                 u'node_to': 3,
                 u'owner': u'player_1',
-                u'points': u'100'
+                u'points': 100
             }
         ]})
+
+    def test_check_one_invalid_move_points_overage(self):
+        """
+        """
+        board, moves_processed, moves_with_errors = engine.process_move(
+            '''{"nodes" : [{
+                  "id" : 1,
+                  "owner" : "player_1",
+                  "points" : "100",
+                  "connections" : [2],
+                  "starting_node" : "player_1"
+                }, {
+                  "id" : 2,
+                  "owner" : "player_2",
+                  "points" : "100",
+                  "connections" : [1],
+                  "starting_node" : "player_2"
+                }]}''',
+            '''{"moves" : [{
+                  "node_from" : 1,
+                  "node_to" : 2,
+                  "owner" : "player_1",
+                  "points" : "101"
+                }]}'''
+        )
+
+        self.assertDictEqual(board, {u'nodes': [
+            {
+                u'id': 1,
+                u'owner': u'player_1',
+                u'points': 100,
+                u'connections': [2],
+                u'starting_node': u'player_1'
+            },
+            {
+                u'starting_node': u'player_2',
+                u'connections': [1],
+                u'points': 100,
+                u'owner': u'player_2',
+                u'id': 2
+            }
+        ]})
+
+        self.assertDictEqual(moves_processed, {u'moves': []})
+
+        self.assertDictEqual(moves_with_errors, {u'moves': [
+            {
+                u'node_from': 1,
+                u'node_to': 2,
+                u'owner': u'player_1',
+                u'points': 101
+            }
+        ]})
+
+    def test_check_one_invalid_move_invalid_owner(self):
+        """
+        """
+        board, moves_processed, moves_with_errors = engine.process_move(
+            '''{"nodes" : [{
+                  "id" : 1,
+                  "owner" : "player_1",
+                  "points" : "100",
+                  "connections" : [2],
+                  "starting_node" : "player_1"
+                }, {
+                  "id" : 2,
+                  "owner" : "player_2",
+                  "points" : "100",
+                  "connections" : [1],
+                  "starting_node" : "player_2"
+                }]}''',
+            '''{"moves" : [{
+                  "node_from" : 1,
+                  "node_to" : 2,
+                  "owner" : "player_3",
+                  "points" : "100"
+                }]}'''
+        )
+
+        self.assertDictEqual(board, {u'nodes': [
+            {
+                u'id': 1,
+                u'owner': u'player_1',
+                u'points': 100,
+                u'connections': [2],
+                u'starting_node': u'player_1'
+            },
+            {
+                u'starting_node': u'player_2',
+                u'connections': [1],
+                u'points': 100,
+                u'owner': u'player_2',
+                u'id': 2
+            }
+        ]})
+
+        self.assertDictEqual(moves_processed, {u'moves': []})
+
+        self.assertDictEqual(moves_with_errors, {u'moves': [
+            {
+                u'node_from': 1,
+                u'node_to': 2,
+                u'owner': u'player_3',
+                u'points': 100
+            }
+        ]})
+
+    def test_check_one_valid_move(self):
+        """
+        """
+        board, moves_processed, moves_with_errors = engine.process_move(
+            '''
+            {
+                "nodes" : [
+                    {
+                        "id"            : 1,
+                        "owner"         : "player_1",
+                        "points"        : "100",
+                        "connections"   : [ 2 ],
+                        "starting_node" : "player_1"
+                    },
+                    {
+                        "id"            : 2,
+                        "owner"         : "player_2",
+                        "points"        : "100",
+                        "connections"   : [ 1 ],
+                        "starting_node" : "player_2"
+                    }
+                ]
+            }
+            ''',
+            '''
+            {
+                "moves" : [
+                    {
+                        "node_from" : 1,
+                        "node_to" : 2,
+                        "owner" : "player_1",
+                        "points" : "10"
+                    }
+                ]
+            }'''
+        )
+
+        self.assertDictEqual(board, {u'nodes': [
+            {
+                u'id': 1,
+                u'owner': u'player_1',
+                u'points': 90,
+                u'connections': [2],
+                u'starting_node': u'player_1'
+            },
+            {
+                u'id': 2,
+                u'owner': u'player_2',
+                u'points': 90,
+                u'connections': [1],
+                u'starting_node': u'player_2'
+            }
+        ]})
+
+        self.assertDictEqual(moves_processed, {u'moves': [
+            {
+                u'node_from': 1,
+                u'node_to': 2,
+                u'owner': u'player_1',
+                u'points': 10
+            }
+        ]})
+
+        self.assertDictEqual(moves_with_errors, {'moves': []})
+
+    def test_check_one_valid_move_two_owners_same_target(self):
+        """
+        """
+        board, moves_processed, moves_with_errors = engine.process_move(
+            '''
+            {
+                "nodes" : [
+                    {
+                        "id"            : 1,
+                        "owner"         : "player_1",
+                        "points"        : "100",
+                        "connections"   : [ 2, 3 ],
+                        "starting_node" : "player_1"
+                    },
+                    {
+                        "id"            : 2,
+                        "owner"         : "player_2",
+                        "points"        : "100",
+                        "connections"   : [ 1, 3 ],
+                        "starting_node" : "player_2"
+                    },
+                    {
+                        "id"            : 3,
+                        "owner"         : "player_3",
+                        "points"        : "100",
+                        "connections"   : [ 1, 2 ],
+                        "starting_node" : "player_3"
+                    }
+                ]
+            }
+            ''',
+            '''
+            {
+                "moves" : [
+                    {
+                        "node_from" : 1,
+                        "node_to" : 2,
+                        "owner" : "player_1",
+                        "points" : "100"
+                    },
+                    {
+                        "node_from" : 3,
+                        "node_to" : 2,
+                        "owner" : "player_3",
+                        "points" : "100"
+                    }
+                ]
+            }'''
+        )
+
+        self.assertDictEqual(board, {u'nodes': [
+            {
+                u'id': 1,
+                u'owner': u'player_1',
+                u'points': 0,
+                u'connections': [2, 3],
+                u'starting_node': u'player_1'
+            },
+            {
+                u'id': 2,
+                u'owner': u'player_2',
+                u'points': 0,
+                u'connections': [1, 3],
+                u'starting_node': u'player_2'
+            },
+            {
+                u'id': 3,
+                u'owner': u'player_3',
+                u'points': 0,
+                u'connections': [1, 2],
+                u'starting_node': u'player_3'
+            }
+        ]})
+
+        self.assertDictEqual(moves_processed, {u'moves': [
+            {
+                u'node_from': 1,
+                u'node_to': 2,
+                u'owner': u'player_1',
+                u'points': 100
+            },
+            {
+                u'node_from': 3,
+                u'node_to': 2,
+                u'owner': u'player_3',
+                u'points': 100
+            }
+        ]})
+
+        self.assertDictEqual(moves_with_errors, {'moves': []})
