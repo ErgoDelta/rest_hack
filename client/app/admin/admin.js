9b84155e4daf4a1193f25b606ee2567cf56a8de1
ErgoDelta <ErgoDelta@Gmail.com>
2015-02-24 00:40:29 -0500

    re-refactored the move to node.js using the yoeman full angular stack template. moved all the old stuff to _old_rest_hack. You all get to make your bed now... work in this and get it working. That said you can start by preforming a bower install and an npm install, that should get you mostly up and running.

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('restHackApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
